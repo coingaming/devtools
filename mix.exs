fea0f3362dde6647a09bbc9ecbf1354d82565096
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-05-24 16:44:25 +0300

    version update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Devtools.MixProject do
   def project do
     [
       app: :devtools,
-      version: "1.0.5",
+      version: "1.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
