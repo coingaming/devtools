49c1ed40dd5355c0a828c663455ad5a5fd0076d3
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-05-24 17:08:07 +0300

    add minor

--- lib/mix/tasks/devtools/release.ex
+++ lib/mix/tasks/devtools/release.ex
@@ -2,8 +2,13 @@ defmodule Mix.Tasks.Devtools.Release do
   use Mix.Task
 
   alias Mix.Tasks.Devtools.Common
-  
-  @shortdoc "Prepare for release"
+
+  def run(["--minor"]) do
+    path = "#{Common.path}/minor"
+    {_, 0} = System.cmd(path, ["--minor"]) |> IO.inspect
+  end
+
+  @shortdoc "Prepare for release, use --minor for minor release"
   def run(args) do
     path = "#{Common.path}/release"
     {_, 0} = System.cmd(path, args) |> IO.inspect
