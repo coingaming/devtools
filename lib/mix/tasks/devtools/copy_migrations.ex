b875624cd373d51f5991ba8c47496c09e648eb12
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-06-26 10:56:50 +0300

    add logging

--- lib/mix/tasks/devtools/copy_migrations.ex
+++ lib/mix/tasks/devtools/copy_migrations.ex
@@ -9,8 +9,12 @@ defmodule Mix.Tasks.Devtools.CopyMigrations do
   def run(_args) do
     migrations_copy_to = Path.join(Common.root_path(), Common.migrations_path())
 
+    Logger.info("Common root path: #{Common.root_path()}")
+    Logger.info("CurrentProject: #{current_project}")
+    Logger.info("Copy to: #{migrations_copy_to}")
+
     migrations_copy_from =
-      case Common.root_path() |> String.split("/") |> List.last() do
+      case current_project do
         "loyalty-api" ->
           Path.join(Common.root_path(), "../loyalty-worker")
 
@@ -24,5 +28,9 @@ defmodule Mix.Tasks.Devtools.CopyMigrations do
 
     Logger.info("Copying migrations from #{migrations_copy_from} to #{migrations_copy_to}")
     File.cp_r!(migrations_copy_from, migrations_copy_to)
+  end
+
+  defp current_project do
+    Common.root_path() |> String.split("/") |> List.last()
   end
 end
