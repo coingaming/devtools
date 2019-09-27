c93edbbf9a104a7e36972014920642dbe9bb9119
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-09-27 12:31:46 +0300

    Update version in mix.exs to 1.4.13

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Devtools.MixProject do
   def project do
     [
       app: :devtools,
-      version: "1.4.12",
+      version: "1.4.13",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
