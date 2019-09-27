650c4c73bf587467a4fb5af12f666659290c7b67
Aleksandr <aleksandr@heathmont.net>
2019-09-27 15:03:23 +0300

    fix (#6)

--- lib/mix/tasks/devtools/patch.ex
+++ lib/mix/tasks/devtools/patch.ex
@@ -4,8 +4,8 @@ defmodule Mix.Tasks.Devtools.Patch do
   alias Mix.Tasks.Devtools.Common
 
   @shortdoc "Version patch + tag creation"
-  def run(args) do
+  def run(_args) do
     path = "#{Common.scripts_path()}/patch"
-    {_, 0} = System.cmd(path, args) |> IO.inspect()
+    {_, 0} = System.cmd("bash", [path]) |> IO.inspect()
   end
 end
