a6d95ba98aa47e83108a76aacaffa44c97970cec
Aleksandr <aleksandr@heathmont.net>
2019-12-11 12:49:03 +0200

    dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Devtools.MixProject do
   def project do
     [
       app: :devtools,
-      version: "1.5.4",
+      version: "1.5.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
