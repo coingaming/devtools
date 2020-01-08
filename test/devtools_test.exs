940ceda8f65e0669cc694573068acff45e7adbed
kiote <ekaterinak@heathmont.net>
2020-01-08 15:47:50 +0200

    move setup

--- test/devtools_test.exs
+++ test/devtools_test.exs
@@ -1,19 +1,19 @@
 defmodule DevtoolsTest do
   use ExUnit.Case
 
+  setup_all do
+    on_exit(fn ->
+      System.cmd(
+        "git",
+        ~w[checkout .],
+        stderr_to_stdout: true
+      )
+    end)
+
+    :ok
+  end
+
   describe "pre-release" do
-    setup_all do
-      on_exit(fn ->
-        System.cmd(
-          "git",
-          ~w[checkout .],
-          stderr_to_stdout: true
-        )
-      end)
-
-      :ok
-    end
-
     test "adds pre-release" do
       assert Mix.Tasks.Devtools.Pre.run("v1.0.0") == "v1.0.0-0"
     end
