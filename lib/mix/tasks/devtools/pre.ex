b2f6048398dbf8c4bf1473689b90e5fe3a402ab6
kiote <ekaterinak@heathmont.net>
2020-01-08 15:45:23 +0200

    start devtools with no bash scripts

--- lib/mix/tasks/devtools/pre.ex
+++ lib/mix/tasks/devtools/pre.ex
@@ -1,11 +1,72 @@
 defmodule Mix.Tasks.Devtools.Pre do
   use Mix.Task
 
-  alias Mix.Tasks.Devtools.Common
+  # alias Mix.Tasks.Devtools.Common
 
-  @shortdoc "Version patch + tag creation"
-  def run(_args) do
-    path = "#{Common.scripts_path()}/pre-release"
-    {_, 0} = System.cmd("bash", [path]) |> IO.inspect()
+  defp version("") do
+    case System.cmd(
+           "git",
+           ~w[describe --tags],
+           stderr_to_stdout: true
+         ) do
+      {version, 0} ->
+        process(version)
+
+      _ ->
+        "0.0.0-dev"
+    end
+  end
+
+  defp version(tag) do
+    process(tag)
+  end
+
+  defp process(tag) do
+    tag
+    |> remove_v()
+    |> String.trim()
+    |> Version.parse()
+    |> patch_version()
+    |> to_string()
+
+    # |> add_v_back()
+  end
+
+  defp remove_v("v" <> version), do: version
+  defp remove_v(s), do: s
+
+  defp add_v_back(s), do: "v" <> s
+
+  defp patch_version({:ok, %Version{pre: []} = version}) do
+    struct(version, pre: [0])
+  end
+
+  defp patch_version({:ok, %Version{pre: [_] = p} = version}) do
+    struct(version, pre: [p + 1])
+  end
+
+  defp patch_version({:ok, %Version{} = version}),
+    do: version
+
+  defp replace_version(line, version) do
+    if String.match?(line, ~r/version:\s"\d+\.\d+\.\d+(-\d+)?\"/) do
+      String.replace(line, ~r/version:\s"\d+\.\d+\.\d+(-\d+)?\"/, "version: \"#{version}\"")
+    else
+      line
+    end
+  end
+
+  @shortdoc "Version prerelease + tag creation"
+  def run(tag) do
+    new_version = version(tag)
+
+    File.stream!("mix.exs")
+    |> Stream.map(&replace_version(&1, new_version))
+    |> Stream.into(File.stream!("output.tx"))
+    |> Stream.run()
+
+    File.cp!("output.tx", "mix.exs")
+
+    add_v_back(new_version)
   end
 end
--- test/devtools_test.exs
+++ test/devtools_test.exs
@@ -1,8 +1,27 @@
 defmodule DevtoolsTest do
   use ExUnit.Case
-  doctest Devtools
 
-  test "greets the world" do
-    assert Devtools.hello() == :world
+  describe "pre-release" do
+    setup_all do
+      on_exit(fn ->
+        System.cmd(
+          "git",
+          ~w[checkout .],
+          stderr_to_stdout: true
+        )
+      end)
+
+      :ok
+    end
+
+    test "adds pre-release" do
+      assert Mix.Tasks.Devtools.Pre.run("v1.0.0") == "v1.0.0-0"
+    end
+
+    test "writes pre-release to mix.exs" do
+      Mix.Tasks.Devtools.Pre.run("v1.0.0")
+
+      result = assert String.match?(File.read!("mix.exs"), ~r/version:\s"\d+\.\d+\.\d+(-\d+)?\"/)
+    end
   end
 end
