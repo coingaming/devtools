38b30ce6f02adac04f0b12b24ab2e50c2c57da65
kiote <krivich.ekaterina@gmail.com>
2020-04-21 17:41:55 +0300

    new version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Devtools.MixProject do
   def project do
     [
       app: :devtools,
-      version: "2.0.1",
+      version: "2.0.2",
       elixir: "~> 1.10.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
