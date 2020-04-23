3ea0cb259fad7869a0c5d36002b9195f92398a9d
kiote <krivich.ekaterina@gmail.com>
2020-04-23 11:18:33 +0300

    match commamd result

--- lib/mix/tasks/devtools/docker_build.ex
+++ lib/mix/tasks/devtools/docker_build.ex
@@ -1,11 +1,16 @@
 defmodule Mix.Tasks.Devtools.DockerBuild do
   use Mix.Task
 
+  require Logger
+
   alias Mix.Devtools.Common
 
   @shortdoc "Docker build"
   def run(args) do
     path = "#{Common.scripts_path()}/docker-build"
-    System.cmd(path, args) |> IO.inspect()
+    command_result = System.cmd(path, args)
+    Logger.info(inspect(command_result))
+
+    {_, 0} = command_result
   end
 end
