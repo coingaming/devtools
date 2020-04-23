d0440436bd9c4e7c35f27570c7c9ab2829298fd4
kiote <krivich.ekaterina@gmail.com>
2020-04-23 09:46:08 +0300

    Update version in mix.exs to 2.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Devtools.MixProject do
   def project do
     [
       app: :devtools,
-      version: "2.0.2",
+      version: "2.0.3",
       elixir: "~> 1.10.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
